b0df9009651def2ed5d0c178d8ebbdf836a7c8a7
adrianabarros-nutri <adriana.ribeiro.barros@gmail.com>
2026-08-11 11:37:50 -0300

    Add files via upload

--- anamnese/service-worker.js
+++ anamnese/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='adriana-barros-anamnese-v5-final';
+const CACHE_NAME='adriana-barros-anamnese-v6-final';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
